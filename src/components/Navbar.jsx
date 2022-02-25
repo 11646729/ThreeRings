83beb22d79f656e7bbf8f4cad15c542ae79db7db
2451134 <bds6052@gmail.com>
2022-02-25 21:10:05 +0000

    2nd commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  padding: 10px 20px;
+`
+
+const Navbar = () => {
+  return <Container>Navbar</Container>
+}
+
+export default Navbar
