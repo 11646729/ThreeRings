6fbbc0f73b5f33cd7c605bb62729a60c82a46fcb
2451134 <bds6052@gmail.com>
2022-03-01 10:38:44 +0000

    Big Refactor into frontend and backend directories

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+// import Navbar from "../components/Navbar"
+import ThreeRingsPage from "./ThreeRingsPage"
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Navbar /> */}
+      <ThreeRingsPage />
+    </div>
+  )
+}
+
+export default Home
