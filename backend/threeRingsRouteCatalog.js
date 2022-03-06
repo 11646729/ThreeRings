cdac361931f1a92f766866d2d0f0897ad7965111
2451134 <bds6052@gmail.com>
2022-03-06 10:47:15 +0000

    Refactored to remove unused code & simplify routes

--- backend/threeRingsRouteCatalog.js
+++ backend/threeRingsRouteCatalog.js
@@ -11,17 +11,6 @@ threeRingsRouter.get("/", () => {
 })
 
 // -------------------------------------------------------
-// GET An api endpoint that returns a short list of items
-// -------------------------------------------------------
-// threeRingsRouter.get("/getList", (req, res) => {
-//   console.log("Here")
-
-//   const list = ["item1", "item2", "item3"]
-//   res.json(list)
-//   console.log("sent list of items")
-// })
-
-// -------------------------------------------------------
 // Function to fetch all 3Rings Shift data
 // -------------------------------------------------------
 threeRingsRouter.get("/shifts", async (req, res) => {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,12 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import List from "./pages/List"
-// import ThreeRingsPage from "./pages/ThreeRingsPage"
+import ThreeRingsPage from "./pages/ThreeRingsPage"
 
 const App = () => {
   return (
     <div>
       <Routes>
-        {/* <Route exact path="/" element={<ThreeRingsPage />} /> */}
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/list" element={<List />} />
-        <Route path="*" element={<Home />} />
+        <Route exact path="*" element={<ThreeRingsPage />} />
       </Routes>
     </div>
   )
